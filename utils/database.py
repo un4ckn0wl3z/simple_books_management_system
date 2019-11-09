1bf71e4edcdb6587bfd0d605639b1d30293d663b
un4ckn0wl3z <slickerth@gmail.com>
2019-11-10 00:21:50 +0700

    add type hinting

--- utils/database.py
+++ utils/database.py
@@ -1,19 +1,21 @@
+from typing import List, Dict, Union
 from .database_connection import DatabaseConnection
 from exceptions.duplicate_book_exception import DuplicateBookException
 
 """
 Concern with storing and retrieving books from a json file.
 """
+Book = Dict[str, Union[str, int]]
 db = 'data.db'
 
 
-def create_book_table():
+def create_book_table() -> None:
     with DatabaseConnection(db) as connection:
         cursor = connection.cursor()
         cursor.execute('CREATE TABLE IF NOT EXISTS books(name text primary key, author text, read integer)')
 
 
-def add_book(name, author):
+def add_book(name: str, author: str) -> None:
     with DatabaseConnection(db) as connection:
         cursor = connection.cursor()
         try:
@@ -22,7 +24,7 @@ def add_book(name, author):
             raise DuplicateBookException('This book has already in database.', 500)
 
 
-def get_all_book():
+def get_all_book() -> List[Book]:
     with DatabaseConnection(db) as connection:
         cursor = connection.cursor()
         cursor.execute('SELECT * FROM books')
@@ -30,13 +32,13 @@ def get_all_book():
     return books
 
 
-def mark_book_as_read(name):
+def mark_book_as_read(name: str) -> None:
     with DatabaseConnection(db) as connection:
         cursor = connection.cursor()
         cursor.execute('UPDATE books SET read=1 WHERE name=?', (name,))
 
 
-def delete_book(name):
+def delete_book(name: str) -> None:
     with DatabaseConnection(db) as connection:
         cursor = connection.cursor()
         cursor.execute('DELETE FROM books WHERE name=?', (name,))
