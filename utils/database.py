45ec5362ec921d76129afc56fb0adf8204c25e6e
un4ckn0wl3z <slickerth@gmail.com>
2019-11-09 15:10:47 +0700

    init in memory feature

---
+++ utils/database.py
@@ -0,0 +1,23 @@
+"""
+Concern with storing and retrieving books from a list
+"""
+books = []
+
+
+def add_book(name, author):
+    books.append({"name": name, "author": author, "read": False})
+
+
+def get_all_book():
+    return books
+
+
+def mark_book_as_read(name):
+    for book in books:
+        if book["name"] == name:
+            book["read"] = True
+
+
+def delete_book(name):
+    global books
+    books = [book for book in books if book["name"] != name]
